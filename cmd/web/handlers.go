abde51800a90df0e7603342696e0d355c0c3965e
Babu Macharia <babu.macharia@gmail.com>
2024-03-14 13:41:19 -0500

    serving static files pg 57

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -14,13 +14,22 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	ts, err := template.ParseFiles("./ui/html/home.page.tmpl")
+	//Intialize a slice containing the paths to the two files
+	// the file containng our base template must be the **first** file in the slice
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/partials/nav.tmpl",
+		"./ui/html/pages/home.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
-	err = ts.Execute(w, nil)
+	// the ExecuteTemplate methid is used to write the content of the "base" template
+	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,8 +7,15 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
+	// Create a file server which serves files out of the "./ui/static" directory.
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+
+	// Use the mux.Handle() function to register the file server as the handler for all URL paths that start with "/static/".
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", snippetView)
+	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Println("Starting server on :4000")

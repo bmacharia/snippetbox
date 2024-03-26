cc7bb99c74cfd068b1b857ba75cd3030b204ac4a
Babu Macharia <babu.macharia@gmail.com>
2024-03-26 16:33:35 -0700

    implement Multiple-record SQL queries

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"html/template"
 	"net/http"
 	"strconv"
+
+	"snippetbox.bmacharia/internal/models"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -13,25 +15,36 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	//Intialize a slice containing the paths to the two files
-	// the file containng our base template must be the **first** file in the slice
-	files := []string{
-		"./ui/html/base.tmpl",
-		"./ui/html/partials/nav.tmpl",
-		"./ui/html/pages/home.tmpl",
-	}
-
-	ts, err := template.ParseFiles(files...)
+	snippets, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, r, err)
 		return
 	}
-	// the ExecuteTemplate methid is used to write the content of the "base" template
-	err = ts.ExecuteTemplate(w, "base", nil)
-	if err != nil {
-		app.serverError(w, r, err)
+
+	for _, snippet := range snippets {
+		fmt.Fprintf(w, "%v\n", snippet)
 	}
 }
+
+//Intialize a slice containing the paths to the two files
+// the file containng our base template must be the **first** file in the slice
+//	files := []string{
+//		"./ui/html/base.tmpl",
+//		"./ui/html/partials/nav.tmpl",
+//		"./ui/html/pages/home.tmpl",
+//	}
+//
+//	ts, err := template.ParseFiles(files...)
+//	if err != nil {
+//		app.serverError(w, r, err)
+//		return
+//	}
+//	// the ExecuteTemplate methid is used to write the content of the "base" template
+//	err = ts.ExecuteTemplate(w, "base", nil)
+//	if err != nil {
+//		app.serverError(w, r, err)
+//	}
+//}
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
@@ -40,7 +53,17 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+	snippet, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+		} else {
+			app.serverError(w, r, err)
+		}
+		return
+	}
+
+	fmt.Fprintf(w, "%+v", snippet)
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"database/sql"
+	"errors"
 	"time"
 )
 
@@ -40,11 +41,64 @@ func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
 }
 
 // function to get a specific snippet based on its ID
-//func (m *SnippetModel) Get(id int) (Snippet, error) {
-//	return nil, nil
-//}
+func (m *SnippetModel) Get(id int) (Snippet, error) {
+
+	// write the sql statement that we want to execute
+	stmt := `SELECT id, title, content, created, expires FROM snippets
+	WHERE expires > UTC_TIMESTAMP() AND id = ?`
+	// use the QueryRow() method on the connection pool to execute our to execute our SQL statement
+	// this returns a pointer to a sql.Row object which holds the result from the database
+	row := m.DB.QueryRow(stmt, id)
+
+	// z new zeroed Snippet struct
+	var s Snippet
+
+	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+	if err != nil {
+		// if the query returns no rows from the database, then row.Scan() will return a sql.ErrNoRows error
+		// that error is handle by the errors.Is() function
+		if errors.Is(err, sql.ErrNoRows) {
+			return Snippet{}, ErrNoRecord
+		} else {
+			return Snippet{}, err
+		}
+	}
+	// if everything worked as expected, then return the Snippet data as normal
+	return s, nil
+}
 
 // function to get the 10 most recently created snippets
 func (m *SnippetModel) Latest() ([]Snippet, error) {
-	return nil, nil
+	stmt := `SELECT id, title, content, created, expires FROM snippets
+	WHERE expires > UTC_TIMESTAMP() ORDER BY created DESC LIMIT 10`
+
+	// use the Query() method on the connection pool to execute our SQL statement
+	rows, err := m.DB.Query(stmt)
+	if err != nil {
+		return nil, err
+	}
+
+	// we defer rows.Close() to ensure that the result set is always properly closed before the Latest() method returns
+	defer rows.Close()
+
+	// a slice of Snippet struct to hold the data
+	var snippets []Snippet
+
+	for rows.Next() {
+		var s Snippet
+
+		err = rows.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+
+		if err != nil {
+			return nil, err
+		}
+
+		snippets = append(snippets, s)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return snippets, nil
 }
